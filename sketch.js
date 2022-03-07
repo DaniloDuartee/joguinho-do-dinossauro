38bfa3fda92afa959c3d7e926811fdeecd9bff91
DaniloDuartee <100432386+DaniloDuartee@users.noreply.github.com>
2022-03-07 19:41:42 -0300

    coloquei imagem na nuvem e no cacto

--- sketch.js
+++ sketch.js
@@ -1,49 +1,34 @@
-// crio as variaveis
-var trex, trexCorrendo 
-var bordas
-var chao,imagemChao, chaoInvisivel
+var caixa;
 
-// serve para precarregar imagens/animacoes/sons
-function preload(){
-    trexCorrendo = loadAnimation('trex1.png', 'trex2.png', 'trex3.png')
-    imagemChao = loadImage("ground2.png")
-}
-
-
-// serve pra fazer a configuracao inicial (só é executada 1 vez quando o jogo começar)
 function setup() {
-    createCanvas(600, 200)
-
-    trex = createSprite(50, 120, 20, 50)
-    trex.addAnimation('correndo', trexCorrendo)
-    trex.scale = 0.7
-
-    chao = createSprite(300,190,600,20)
-    chao.addImage(imagemChao)
-    chao.x = chao.width/2
-    chaoInvisivel = createSprite(300,205,600,10)
-    chaoInvisivel.visible = false
-
-    bordas = createEdgeSprites()
+  createCanvas(540,540);
+  caixa = createSprite(200,200,30,30)
 }
 
-// serve para fazer o jogo funcionar o tempo todo (é executada o tempo todo, infinitamente até eu parar o jogo)
 function draw() {
-    background('white')
+background(30);
 
-    if (keyDown('space')) {
-        trex.velocityY = -10
-    }
 
-    trex.velocityY = trex.velocityY + 0.5
-
-    chao.velocityX = -4
-
-    if(chao.x <0){
-        chao.x = chao.width/2
-    }
-
-    trex.collide(chaoInvisivel)
-
-    drawSprites()
+if(keyDown("right_arrow")){
+    background("red")
+    caixa.x = caixa.x +5
 }
+
+if(keyDown("left_arrow")){
+  caixa.x = caixa.x -5
+}
+
+if(keyDown("down_arrow")){
+  caixa.y = caixa.y +5
+}
+
+  if(keyDown("up_arrow")){
+  caixa.y = caixa.y -5
+}
+
+  drawSprites();
+}
+
+
+
+
